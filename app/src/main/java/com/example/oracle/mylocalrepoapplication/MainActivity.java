03901c725f9fbb7341659b9de23e36105aa42656
eudj465 <eudj123@naver.com>
2017-06-19 14:32:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/oracle/mylocalrepoapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.oracle.mylocalrepoapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+*   this is a MainActivity
+* */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
